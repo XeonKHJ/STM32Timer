7dc844ad4b272a98433cbddacc5284faad60d53c
Hongjia <redalertkhj@live.cn>
2019-12-23 00:45:22 +0800

    正在写框架。

--- Source/main.c
+++ Source/main.c
@@ -1,4 +1,25 @@
+#include "Common.h"
+
+void CountDown();
+
 int main()
 {
+    ShowMenu();
+    int functionNo = GetSelectedFuntion();
+    switch (functionNo)
+    {
+    case 1:
+        CountDown();
+        break;
+    case 2:
+        Timer();
+    default:
+        Exit();
+    }
+}
+
+void CountDown()
+{
+    InitalCount = GetInitialTime();
     
 }
